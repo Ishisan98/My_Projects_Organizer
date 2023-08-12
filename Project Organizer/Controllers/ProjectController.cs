c37da3c20efc77bb10178df18f15a7bc3e560183
Wathanta Aung <wathanta.aung@interns.eonreality.com>
2023-08-12 15:43:36 +0800

    First Commit

---
+++ Project Organizer/Controllers/ProjectController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_Organizer.Models;
+
+namespace Project_Organizer.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Dashboard ()
+        {
+            return View ();
+        }
+
+        
+        public IActionResult CreateProject()
+        {
+            return View ();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProject(Project projectForm)
+        {
+            Project newProject = new Project();
+
+            newProject.Project_Id = projectForm.Project_Id;
+            newProject.Project_Name = projectForm.Project_Name;
+            newProject.CreatedDate = DateTime.Now;
+            newProject.ProjectStatus = true;
+
+            return View("Dashboard");
+        }
+    }
+}
---
+++ Project Organizer/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿namespace Project_Organizer.Models
+{
+    public class Project
+    {
+        public int Project_Id { get; set; }
+
+        public string Project_Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public bool ProjectStatus { get; set; }
+    }
+}
