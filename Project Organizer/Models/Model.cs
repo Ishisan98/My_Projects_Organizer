7014e4f781572988538dc8225a90b5a4654a4ccc
Wathanta Aung <wathanta.aung@interns.eonreality.com>
2023-08-27 16:20:13 +0800

    Updated Update_Project html

---
+++ Project Organizer/Models/Model.cs
@@ -0,0 +1,13 @@
+﻿namespace Project_Organizer.Models
+{
+    public class Model
+    {
+        public int Model_Id { get; set; }
+
+        public string Model_Name { get; set; }
+
+        public string? Description { get; set;}
+
+        public int Project_Id { get; set; }
+    }
+}
